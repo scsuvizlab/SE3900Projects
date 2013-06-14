af68bf670e10cfe30654dfdae3d4a7f1aac105fa
Isaac Osei <Oseix004@crk.umn.edu>
2013-06-14 13:48:24 -0500

    working
    Still working on figuring this i understand how it works took a while
    but am still trying different things

---
+++ CameraVisualizations/CameraVisualizations/CamerVisualization.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+
+namespace CameraVisualizations
+{
+    /// <summary>
+    /// Interaction logic for CamerVisualization.xaml
+    /// </summary>
+    public partial class CamerVisualization : TagVisualization
+    {
+        public CamerVisualization()
+        {
+            InitializeComponent();
+        }
+
+        private void CamerVisualization_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO: customize CamerVisualization's UI based on this.VisualizedTag here
+        }
+    }
+}
---
+++ CameraVisualizations/CameraVisualizations/SurfaceWindow1.xaml.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+
+namespace CameraVisualizations
+{
+    /// <summary>
+    /// Interaction logic for SurfaceWindow1.xaml
+    /// </summary>
+    public partial class SurfaceWindow1 : SurfaceWindow
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SurfaceWindow1()
+        {
+            InitializeComponent();
+
+            // Add handlers for window availability events
+            AddWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Occurs when the window is about to close. 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Remove handlers for window availability events
+            RemoveWindowAvailabilityHandlers();
+        }
+
+        /// <summary>
+        /// Adds handlers for window availability events.
+        /// </summary>
+        private void AddWindowAvailabilityHandlers()
+        {
+            // Subscribe to surface window availability events
+            ApplicationServices.WindowInteractive += OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive += OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable += OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// Removes handlers for window availability events.
+        /// </summary>
+        private void RemoveWindowAvailabilityHandlers()
+        {
+            // Unsubscribe from surface window availability events
+            ApplicationServices.WindowInteractive -= OnWindowInteractive;
+            ApplicationServices.WindowNoninteractive -= OnWindowNoninteractive;
+            ApplicationServices.WindowUnavailable -= OnWindowUnavailable;
+        }
+
+        /// <summary>
+        /// This is called when the user can interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowInteractive(object sender, EventArgs e)
+        {
+            //TODO: enable audio, animations here
+        }
+
+        /// <summary>
+        /// This is called when the user can see but not interact with the application's window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowNoninteractive(object sender, EventArgs e)
+        {
+            //TODO: Disable audio here if it is enabled
+
+            //TODO: optionally enable animations here
+        }
+
+        /// <summary>
+        /// This is called when the application's window is not visible or interactive.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnWindowUnavailable(object sender, EventArgs e)
+        {
+            //TODO: disable audio, animations here
+        }
+        private void OnVisualizationAdded(object sender, TagVisualizerEventArgs e)
+        {
+            CamerVisualization camera = (CamerVisualization)e.TagVisualization;
+            switch (camera.VisualizedTag.Value)
+            {
+                case 1:
+                    camera.CameraModel.Content = "JOhn Snow";
+                    camera.myEllipse.Fill = SurfaceColors.Accent1Brush;
+                    break;
+
+                case 2:
+                    camera.CameraModel.Content = "Ned Snow, Games of thrones";
+                    camera.myEllipse.Fill = SurfaceColors.Accent2Brush;
+                    break;
+
+                case 3:
+                    camera.CameraModel.Content = "Snow, Games of thrones";
+                    camera.myEllipse.Fill = SurfaceColors.Accent3Brush;
+                    break;
+
+                case 4:
+                    camera.CameraModel.Content = "Winter is coming, Games of thrones";
+                    camera.myEllipse.Fill = SurfaceColors.Accent4Brush;
+                    break;
+
+                default:
+                    camera.CameraModel.Content = "Winter soldier";
+                    camera.myEllipse.Fill = SurfaceColors.ControlAccentBrush;
+                    break;
+
+            }
+
+        }
+    }
+}
